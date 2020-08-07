2d585550421fdddccffd67ea244bb1cb1bd17d3e
Yashdew <yashdewangan123456@gmail.com>
2020-08-07 18:15:20 +0530

    Insert have bug

--- user/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from app import app          ### importing app function from app.py
-from user.models import User ### calling User Class from user/model.py dir
-
-@app.route('/users/signup', methods=['POST'])   ### rounding to this directory for GET data
-
-def signup():
-    
-    return User().signup()
