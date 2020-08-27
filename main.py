2a8ba6cdba771f439c5c4faa1b0ebd6abcebeb33
Yashdew <yashdewangan123456@gmail.com>
2020-08-27 19:19:29 +0530

    prelogin update function done

--- main.py
+++ main.py
@@ -1,88 +1,37 @@
 import pymongo
-from pymongo import MongoClient
 
 myclient = pymongo.MongoClient("mongodb+srv://yashdew:5kAa9bRquer0cRtq@cluster0.c6ung.mongodb.net/yashdew?retryWrites=true&w=majority")
-                                                    
-mydb = myclient["Yashdb"]
-mycol = mydb["Yashdb"]
-var = "jgdjkAHKJ@gmail.com"
+
+mydb = myclient["Yashdb1"]
+mycol = mydb["Yashdb1"]
+
 for x in mycol.find():
-    if x['email']==var:
-           print(x['_id'])
-           print(x['name'])
-           print(x['username'])
-           print(x['email'])
-
+    if x['Email']=='kul@gmail.com' and x['Password']=='bhaibhai':
+        record=x
+        y=record['_id']
+        print(record['_id'])
+        """newrecord = {
+                    "$set",{
+                        "Email":request.form['NEmail'],
+                        "Name":request.form['MName'],
+                        "Password":request.form['NPassword'],
+                        "CPassword":request.form['NPassword'],
+                        "Period1":{
+                            "Dept":request.form['Department'],
+                            "Year":request.form['Year'],
+                            "Subject":request.form['Subject'],
+                            "Div":request.form['Div'],
+                            
+                        }
+                    }
+        }"""
+        newrecord={"$set":{"Email":'kul@gmail.com',"Name":"bhaibhai","Password":"bhaibhai","CPassword":"bhaibhai","Period1":{
+                            "Dept":"IT",
+                            "Year":"FE",
+                            "Subject":"DBMS",
+                            "Div":"Div2",
+                            
+                        }}}
+                
+mycol.update_many({"_id":record['_id']},newrecord)
             
-            from flask_restful import Resource
-import pandas as pd
-
-ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-
-class UploadCSV(Resource):
-
-    def post(self):
-        files = request.files['file']
-        files.save(os.path.join(ROOT_PATH,files.filename))
-        data = pd.read_csv(os.path.join(ROOT_PATH,files.filename))
-        print(data)
-
-api.add_resource(UploadCSV, '/v1/upload')
-
-if __name__ == '__main__':
-    app.run(host='localhost', debug=True, port=5000)
-
-
-
-
-    ########################################################################
-
-    filename=request.form['myfile']
-
-    filename.save(os.path.join(ROOT_PATH,filename.filename))
-
-    print(request.form['myfile'])
-
-    df = pd.read_csv(os.path.join(ROOT_PATH,filename.filename))
-
-    data = df.to_dict('records')
-    mycol1.insert_many(data)
-    return render_template('download.html')
-
-
-    """Period5":{
-                                            "Dept":none,
-                                            "Subject":none,
-                                            "Div":none
-
-                            },
-                            "Period6":{
-                                            "Dept":none,
-                                            "Subject":none,
-                                            "Div":none
-
-                            },
-                            "Period7":{
-                                            "Dept":none,
-                                            "Subject":none,
-                                            "Div":none
-
-                            },
-                            "Period8":{
-                                            "Dept":none,
-                                            "Subject":none,
-                                            "Div":none
-
-                            },
-                            "Period9":{
-                                            "Dept":none,
-                                            "Subject":none,
-                                            "Div":none
-
-                            },
-                            "Period10":{
-                                            "Dept":none,
-                                            "Subject":none,
-                                            "Div":none
-
-                            }"""
